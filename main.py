4104f5680de8dc671255120a6f5cffd341831f73
Jose Ewerton da Cruz de Souza <78435817+jose-ewerton@users.noreply.github.com>
2022-01-31 18:47:31 +0000

    Apliquei a aula 02

---
+++ main.py
@@ -0,0 +1,111 @@
+#aula passada input() e print()
+
+'''
+input() - entrada
+
+input(String)
+
+print() - saída
+
+print(variavel)
+print(String)
+print(String + variavel)
+print(formatação)
+etc...
+
+'''
+
+#Tipos de dados básicos do Python
+
+#type() - permite saber qual o tipo de variavel que estou recebendo, tratando etc.
+
+# Inteiro
+# 2, 1000, 222, 3435, -1 , -9 ...
+#print(type(n))
+
+# Float 
+# 2.1 ; 2000.9 ; -22; ...
+#print(type(x))
+
+
+
+
+
+
+
+
+#operação de casting ou conversão
+# int() - converte um valor para inteiro 
+# float() - converte um valor para float
+#str() - converte um valor para String 
+
+
+#help() - mostra todas as coisas/operações que eu posso fazer com aquele objeto que estão prontas no python
+
+#Questão 01
+'''
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite mais um número: '))
+somar = n1 + n2
+print('O resultado foi: ',somar)
+
+
+#Questão 01
+try: #tente primeiro fazer isso aqui
+  n1 = float(input('Digite um número: '))
+  n2 = float(input('Digite mais um número: '))
+  somar = n1 + n2
+  print('O resultado foi: ',somar)
+except ValueError: #informar ao usuário que o programa falhou por causa de alguma entrada inválida
+  print('Entrada inválida!')
+  print('Eu sou aceito números inteiros e float meu amigo!')
+'''
+
+
+#Questão 02
+nome_completo = input('Digite seu nome completo por favor: ')
+print('Seu nome completo é: ', nome_completo)
+
+
+
+#questão 04
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite mais um número: '))
+sub = n1 - n2
+print('O resultado foi: ',sub)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#String
+
+
+
+#Boolean
+
+
+
+#Comentários
+
+
+
+
